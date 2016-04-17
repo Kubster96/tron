d3a1155b72213468839b70d575644411c733e5b6
Kubster96 <jungiewiczjakub@gmail.com>
2016-04-17 20:53:31 +0200

    Update church.py

--- ZAD2/church.py
+++ ZAD2/church.py
@@ -1,32 +1,51 @@
-##############################################
-# Liczebniki Churcha
-# Jakub Jungiewicz	
-##############################################
+"""
+Liczebniki Churcha
+Jakub Jungiewicz	
+"""
 
-# Liczebniki Churcha
+#funkcja
+function = lambda x: x+1
 
-def zero(f):
-    return lambda x: x
+"""
+Liczebniki Churcha
+"""
+zero=lambda f: lambda x: x
+one=lambda f: lambda x: f(x)
+two=lambda f: lambda x: f(f(x))
+three=lambda f: lambda x: f(f(f(x)))
+four=lambda f: lambda x: f(f(f(f(x))))
+five=lambda f: lambda x: f(f(f(f(f(x)))))
 
-def one(f):
-    return lambda x: f(x)
-
-def two(f):
-    return lambda x: f(f(x))
-
-# Operacje arytmetyczne
-
-def successor(n):
+"""
+Operacje arytmetyczne
+"""
+#nastepnik
+def succ(n):
     return lambda f: lambda x: f(n(f)(x))
-
+    
+#dodawanie
 def add(n,m):
 	return lambda f: lambda x: m(f)(n(f)(x))
 
+#mnozenie
 def mult(n,m):
 	return lambda f: lambda x: n(m(f))(x)
 
+#potegowanie
 def exp(n,m):
 	return n(m)
 
-def church2int(n):
-	return n(lambda x:x+1)(0)
+#wyonywanie i wypisywanie
+def printAndEval(x):
+	print x, ' = ', eval(x)
+
+#zamiana church na int
+def church_to_int(n):
+	return n(function)(0)
+
+#testowanie
+
+printAndEval('church_to_int(succ(one))')
+printAndEval('church_to_int(mult(two, three))')
+printAndEval('church_to_int(add(one, four))')
+printAndEval('church_to_int(exp(four, two))')
