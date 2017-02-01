f2c7b67da9194de85a132db15ce24ff706173c11
Kubster96 <jungiewiczjakub@gmail.com>
2017-02-01 10:29:56 +0100

    Create Menu.java

---
+++ Menu.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+//klasa Menu posiada Panel Przyciskow ktory jest wyswietlany na ekranie
+public class Menu extends JFrame {
+
+    Buttons buttons;
+
+    public Menu(){
+
+        buttons = new Buttons(this);
+        add(buttons);
+
+        setResizable(false);
+        pack();
+        setTitle("Tron - menu");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setFocusable(true);
+        addKeyListener(new KAdapter());
+
+    }
+
+    private class KAdapter extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            buttons.board.keyPressed(e);
+
+        }
+    }
+}
